aa7ba2834369b1ac7e0e1af9a03d44848ab4bcba
ITLAB <ITLAB@IT217>
2017-02-07 14:04:38 +0530

    Fifth

--- src/com/example/graphics/MainActivity.java
+++ src/com/example/graphics/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.graphics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -13,8 +15,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button1);
+        b2=(Button)findViewById(R.id.button2);
+        b3=(Button)findViewById(R.id.button3);
+        
+        b1.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+               
+            }
+         });
+
     }
-    
-    
-    
 }
