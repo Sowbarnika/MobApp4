db289563d7d6029ca70ed35390a3c2ed0a9031b3
ITLAB <ITLAB@IT217>
2017-02-07 16:24:29 +0530

    Eight

--- src/com/example/graphics/MainActivity.java
+++ src/com/example/graphics/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.graphics;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
- 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
- 
-	
- 
+   Button b1,b2;
 
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      new ProgressDialog(this);
+
+      b1=(Button)findViewById(R.id.button1);
+      b2=(Button)findViewById(R.id.button2);
+      b1.setOnClickListener(new View.OnClickListener() {
+         
+         @Override
+         public void onClick(View v) {
+            TextView txtView = (TextView) findViewById(R.id.textView1);
+            txtView.toString();
+         }
+      });
+
+      b2.setOnClickListener(new View.OnClickListener() {
+         
+         @Override
+         public void onClick(View v) {
+            TextView txtView = (TextView) findViewById(R.id.textView1);
+            txtView.setTextSize(55);
+         }
+      });
+   }
 }
